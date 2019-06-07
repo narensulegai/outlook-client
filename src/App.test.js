abed44a920b6ed4a1ea1ca9dd630a014bea73cf7
Naren Sulegai <86naren@gmail.com>
2019-06-07 13:31:28 +0530

    Adding tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+
+import { render } from '@testing-library/react';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it('renders app name', () => {
+  const { getByText } = render(<App />);
+  expect(getByText('Outlook Client')).toBeInTheDocument();
+});
+
+it('renders login message', () => {
+  const { getByText } = render(<App />);
+  expect(getByText('Please log in to Outlook')).toBeInTheDocument();
 });
